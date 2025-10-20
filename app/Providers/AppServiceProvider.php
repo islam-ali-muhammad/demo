3212c73cbe4d85d19fc73a9943097b9f8c68092f
Islam Ali <islam.ali.muhammad@gmail.com>
2025-10-20 17:58:19 +0300

    intialize tenancy by domain in app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Filament::serving(function () {
+            if (! tenancy()->initialized && request()->getHost() !== config('tenancy.central_domains')[0]) {
+                app(InitializeTenancyByDomain::class)->handle(request(), fn($r) => $r);
+            }
+        });
     }
 }
